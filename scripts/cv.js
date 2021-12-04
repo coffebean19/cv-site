c3cb996f5b1e428135f0bdb68c79ea5ec273e025
Coffebean <istvan.mayer1999@protonmail.com>
2021-12-04 22:15:20 +0200

    Fixed animations, now it looks good

--- scripts/cv.js
+++ scripts/cv.js
@@ -3,4 +3,4 @@ const project = document.getElementById('project-card');
 const timeline = document.getElementById('timeline-card');
 
 project.style.setProperty('--animate-duration', '1.2s');
-timeline.style.setProperty('--animate-duration', '1.2s');
+// timeline.style.setProperty('--animate-duration', '1s');
