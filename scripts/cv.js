eddfd62a1897abced29a9a9c651086c94c7028d4
Coffebean <istvan.mayer1999@protonmail.com>
2021-12-04 21:38:24 +0200

    Added some animations, which needs fixing.

---
+++ scripts/cv.js
@@ -0,0 +1,6 @@
+const tech = document.getElementById('tech-card');
+const project = document.getElementById('project-card');
+const timeline = document.getElementById('timeline-card');
+
+project.style.setProperty('--animate-duration', '1.2s');
+timeline.style.setProperty('--animate-duration', '1.2s');
