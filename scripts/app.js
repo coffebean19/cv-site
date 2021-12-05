864fc1c71a4522339dec57de8bdbf76b13bf3f91
Coffebean <istvan.mayer1999@protonmail.com>
2021-12-05 13:29:09 +0200

    Made nav-bar sticky, and added a progress bar to website

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,12 @@ function openNav() {
       x.className = "nav";
     }
   } 
+
+window.onscroll = function() { scrollIndicate() };
+
+function scrollIndicate() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+} 
