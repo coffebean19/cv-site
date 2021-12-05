80acca3173b6dc8ca3f4ae4ebef9e7d9bba7bdca
Coffebean <istvan.mayer1999@protonmail.com>
2021-12-05 14:06:52 +0200

    Added accordion function to cv part

--- scripts/app.js
+++ scripts/app.js
@@ -15,3 +15,18 @@ function scrollIndicate() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 } 
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active-accordion");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
