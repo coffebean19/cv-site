4cbe2fb8e63928eb700f8cefbe451cd0c4f30b0b
coffebean19 <istvan.mayer1999@protonmail.com>
2023-02-13 17:01:21 +0200

    Revamp of entire site with Vue.

--- scripts/cv.js
+++
@@ -1,8 +0,0 @@
-const tech = document.getElementById('tech-card');
-const project = document.getElementById('project-card');
-const timeline = document.getElementById('timeline-card');
-const link = document.getElementById('link');
-
-link.style.setProperty('--animate-duration', '0.8s');
-project.style.setProperty('--animate-duration', '1.2s');
-// timeline.style.setProperty('--animate-duration', '1s');
