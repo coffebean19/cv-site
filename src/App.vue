a5d2243c1cf9284e4d1f76463023b46f955cbc5b
coffebean19 <istvan.mayer1999@protonmail.com>
2024-01-18 22:11:04 +0200

    Started with landing element.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/counter">Counter</RouterLink>
-  </nav>
+  <div v-if="landing" style="transition: opacity 0.5s;">
+    <h1>This is the intro page. Or something</h1>
+    <button class="button-65" @click="landing = !landing">Enter</button>
+  </div>
+  <div v-if="!landing">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/counter">Counter</RouterLink>
+    </nav>
 
-  <RouterView></RouterView>
+    <RouterView></RouterView>
 
-  <header>
-    <div class="wrapper">
-      <Random random="This works" />
-    </div>
-  </header>
+    <header>
+      <div class="wrapper">
+        <Random random="This works" />
+      </div>
+    </header>
 
-  <main>
-    <SecondRandom msg="Wowww" />
-
-  </main>
+    <main>
+      <SecondRandom msg="Wowww" />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Random from './components/Random.vue'
-import SecondRandom from './components/SecondRandom.vue';
+import Random from './views/Random.vue'
+import SecondRandom from './views/SecondRandom.vue';
+import { reactive, ref } from "vue";
+
+//Landing page
+const landing = ref(true);
+
 </script>
 
 <style scoped>
@@ -28,5 +38,66 @@ nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
+}
+
+.button-65 {
+  appearance: none;
+  backface-visibility: hidden;
+  background-color: #2f80ed;
+  border-radius: 10px;
+  border-style: none;
+  box-shadow: none;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Inter, -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif;
+  font-size: 15px;
+  font-weight: 500;
+  height: 50px;
+  letter-spacing: normal;
+  line-height: 1.5;
+  outline: none;
+  overflow: hidden;
+  padding: 14px 30px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transform: translate3d(0, 0, 0);
+  transition: all .3s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: top;
+  white-space: nowrap;
+}
+
+.button-65:hover {
+  background-color: #1366d6;
+  box-shadow: rgba(0, 0, 0, .05) 0 5px 30px, rgba(0, 0, 0, .05) 0 1px 4px;
+  opacity: 1;
+  transform: translateY(0);
+  transition-duration: .35s;
+}
+
+.button-65:hover:after {
+  opacity: .5;
+}
+
+.button-65:active {
+  box-shadow: rgba(0, 0, 0, .1) 0 3px 6px 0, rgba(0, 0, 0, .1) 0 0 10px 0, rgba(0, 0, 0, .1) 0 1px 4px -1px;
+  transform: translateY(2px);
+  transition-duration: .35s;
+}
+
+.button-65:active:after {
+  opacity: 1;
+}
+
+@media (min-width: 768px) {
+  .button-65 {
+    padding: 14px 22px;
+    width: 176px;
+  }
 }
 </style>
